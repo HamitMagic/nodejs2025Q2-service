ea3e560c2f992de4923820cbfa4e058d2dfd9c5b
HamitMagic <Hamit.Magic@gmail.com>
2025-05-27 19:15:00 +0500

    move resources to folder

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
+  const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT);
+  console.log(`server runs on port ${PORT}`);
 }
 bootstrap();
