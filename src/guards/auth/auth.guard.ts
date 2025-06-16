8964b9bed29675098396d46d8a7108c90583933e
HamitMagic <Hamit.Magic@gmail.com>
2025-06-16 20:39:18 +0500

    add public route to guard

--- src/guards/auth/auth.guard.ts
+++ src/guards/auth/auth.guard.ts
@@ -4,20 +4,30 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { JWT } from 'src/constants/jwtConstants';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
-
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+  
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
+
+    const isPublic = this.reflector.getAllAndOverride<boolean>('IS_PUBLIC_KEY', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
     if (!token) {
       throw new UnauthorizedException();
     }
+    
     try {
       const payload = await this.jwtService.verifyAsync(
         token,
